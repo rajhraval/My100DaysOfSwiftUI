4767f5f3cba004413826678fb6ed0baece9460e2
Raj <rajraval37@gmail.com>
2020-03-08 19:56:45 +0530

    Day 68

---
+++ Project 14 - BucketList/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by RAJ RAVAL on 08/03/20.
+//  Copyright © 2020 Buck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension FileManager {
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
+    }
+    
+}
+
---
+++ Project 14 - BucketList/Project14.md
@@ -0,0 +1,11 @@
+## Project 14 - BucketList - 100DaysOfSwiftUI Challenge
+
+### Project 14 - Instafilter - Day 68 of 100DaysOfSwiftUI
+
+> The only place success comes before work is in the dictionary - Vince Lombardi
+
+Today I learnt about:
+
+- Adding conformance of Comparable Protocol for custom types
+- Writting data to the documents directory using the FileManager
+- Switching view states using Enums
