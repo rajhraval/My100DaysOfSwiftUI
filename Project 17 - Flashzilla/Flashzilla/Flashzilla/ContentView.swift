0ba93bcbc5a624892b13dc1c5b4a3901363544d9
Raj <rajraval37@gmail.com>
2020-03-26 12:19:08 +0530

    Day 86

---
+++ Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by RAJ RAVAL on 26/03/20.
+//  Copyright © 2020 Buck. All rights reserved.
+//
+
+import SwiftUI
+import CoreHaptics
+
+struct ContentView: View {
+    
+    @State private var engine: CHHapticEngine?
+    
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("VStack Tapped!")
+        }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine; \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 17 - Flashzilla/Project17.md
@@ -0,0 +1,12 @@
+## Project 17 - Flashzilla - 100DaysOfSwiftUI Challenge
+
+### Project 17 - Flashzilla - Day 86 of 100DaysOfSwiftUI
+
+> When it's feeling really good, sometimes people even say it feels natural, or magical - Chan Karunamuni on gestures
+
+Today I learnt about:
+
+- Introduction and Project Setup
+- Using various gestures in SwiftUI (TapGesture, LongPressGesture, MagnificationGesture, RotationGesture and DragGesture)
+- Working UINotificationFeedbackGenerator and CoreHaptics
+- Disabling UserInteractivity using `allowsHitTesting()` and `contentShape()`
